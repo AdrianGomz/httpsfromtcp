d70279b4c0f73505f2d6797e5b331918295babd9
Isaac Gomez <isaac@MacBook-Pro-de-Isaac.local>
2026-03-08 22:31:36 -0600

    added reading inside goroutine

--- main.go
+++ main.go
@@ -2,31 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(ch)
+
+		curLine := ""
+		for {
+			data := make([]byte, 8)
+			_, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			data_s := string(data)
+			strings := strings.Split(data_s, "\n")
+			curLine += strings[0]
+			if len(strings) > 1 {
+				ch <- curLine
+				curLine = strings[1]
+			}
+		}
+	}()
+	return ch
+
+}
 func main() {
 
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	curLine := ""
-	for {
-		data := make([]byte, 8)
-		_, err := file.Read(data)
-		if err != nil {
-			break
-		}
-		data_s := string(data)
-		strings := strings.Split(data_s, "\n")
-		curLine += strings[0]
-		if len(strings) > 1 {
-			fmt.Printf("read: %s\n", curLine)
-			curLine = strings[1]
-		}
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
-
 }
