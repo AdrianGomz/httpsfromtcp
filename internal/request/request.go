01bc176db9ca111e9026dcc0dd7ba7f3db47b2ff
Isaac Gomez <isaac@MacBook-Pro-de-Isaac.local>
2026-03-10 22:29:22 -0600

    added readding with buffer to keep reading until request compleated

--- internal/request/request.go
+++ internal/request/request.go
@@ -13,6 +13,13 @@ const (
 	HTTP_V1   = "HTTP/1.1"
 )
 
+type parserState string
+
+const (
+	Initialized parserState = "initialized"
+	Done        parserState = "done"
+)
+
 type RequestLine struct {
 	HttpVersion   string
 	RequestTarget string
@@ -23,48 +30,87 @@ type Request struct {
 	RequestLine RequestLine
 	Headers     map[string]string
 	Body        []byte
+	State       parserState
+}
+
+func NewRequest() *Request {
+	return &Request{State: Initialized}
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, fmt.Errorf("unable to io.ReadAll: %w", err)
-	}
-	rl, err := parseRequestLine(strings.Split(string(data), "\r\n")[0])
-	if err != nil {
-		return &Request{}, fmt.Errorf("error parsing reqeust line err: %s", err)
+	request := NewRequest()
+	buffer := make([]byte, 1024)
+	b_insert_idx := 0
+	for request.State != Done {
+		n, err := reader.Read(buffer[b_insert_idx:])
+		if err != nil {
+			return nil, err
+		}
+
+		b_insert_idx += n
+
+		_, err = request.parse(buffer[:b_insert_idx+n])
+		if err != nil {
+			return nil, fmt.Errorf("error parsing the buffer")
+		}
+		// TODO: implement buffer resizing when max capacity exceeded
+
 	}
 
-	return &Request{*rl, nil, nil}, nil
+	return request, nil
 
 }
 
-func parseRequestLine(ms string) (*RequestLine, error) {
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.State {
+	case Initialized:
+		rl, n, err := parseRequestLine(string(data))
+		if err != nil {
+			return 0, err
+		}
+
+		if n == 0 {
+			return 0, nil
+		} else if n > 0 && rl != nil {
+			r.RequestLine = *rl
+			r.State = Done
+		}
+		return n, nil
+
+	case Done:
+		return 0, fmt.Errorf("Request already done")
+
+	default:
+		return 0, fmt.Errorf("Unknown request state")
+	}
+}
+
+func parseRequestLine(ms string) (*RequestLine, int, error) {
 	ln, _, f := strings.Cut(ms, SEPARATOR)
-	if f {
-		return nil, nil
+	if !f {
+		return nil, 0, nil
 	}
 
 	parts := strings.Split(ln, " ")
 
 	if len(parts) != 3 {
-		return &RequestLine{}, fmt.Errorf("error parsing request line")
+		return &RequestLine{}, 0, fmt.Errorf("error parsing request line")
 	}
 	isMatch, err := regexp.MatchString("[A-Z]+", parts[0])
 	if err != nil {
-		return &RequestLine{}, fmt.Errorf("regex error %w", err)
+		return &RequestLine{}, 0, fmt.Errorf("regex error %w", err)
 	}
 	if !isMatch {
-		return &RequestLine{}, errors.New("invalid method")
+		return &RequestLine{}, 0, errors.New("invalid method")
 	}
 	if parts[2] != HTTP_V1 {
-		return &RequestLine{}, errors.New("Unsupported HTTP version")
+		return &RequestLine{}, 0, errors.New("Unsupported HTTP version")
 	}
 
 	return &RequestLine{
 		Method:        parts[0],
 		RequestTarget: parts[1],
 		HttpVersion:   parts[2][5:],
-	}, nil
+	}, len(ms), nil
 
 }
