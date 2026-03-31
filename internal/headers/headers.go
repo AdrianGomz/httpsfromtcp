78598369df60bf07f0bb7791c869fe529a8b0e8a
Isaac Gomez <isaacgom656@gmail.com>
2026-03-31 15:12:22 -0600

    added header name validation

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,6 +3,7 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -15,21 +16,32 @@ type Headers map[string]string
 func NewHeaders() Headers {
 	return Headers{}
 }
+
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 
 	i := bytes.Index(data, []byte(CRLF))
 	if i <= 0 {
-		return 0, i == 0, nil
+		n := 0
+		if i == 0 {
+			n = len(CRLF)
+		}
+		return n, i == 0, nil
 	}
 	ln := string(data[:i])
 	data = data[i+len(CRLF):]
 
 	key, val, _ := strings.Cut(ln, ":")
-	if key != strings.TrimSpace(key) {
+	match, err := regexp.MatchString("^[a-zA-Z!#$%&'*+-.^_`|~]+$", key)
+
+	if err != nil {
+		return 0, false, fmt.Errorf("error parsing headers")
+	}
+
+	if key != strings.TrimSpace(key) || !match {
 		return 0, false, fmt.Errorf("invalid header name")
 	}
 
 	val = strings.TrimSpace(val)
-	h[key] = val
-	return i, true, nil
+	h[key] = strings.ToLower(val)
+	return i + len(CRLF), false, nil
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func testHeaderParsing(t *testing.T) {
+func TestHeaderParsing(t *testing.T) {
 	// Test: Valid single header
 	headers := NewHeaders()
 	data := []byte("Host: localhost:42069\r\n\r\n")
@@ -21,6 +21,41 @@ func testHeaderParsing(t *testing.T) {
 	// Test: Invalid spacing header
 	headers = NewHeaders()
 	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Valid parse header with already existing headers
+	data1 := []byte("Host: localhost:42069\r\n")
+	data2 := []byte("Content-Type: text/html\r\n")
+	n, done, err = headers.Parse(data1)
+	n, done, err = headers.Parse(data2)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "text/html", headers["Content-Type"])
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 2, len(headers))
+
+	// Test: Valid done parsing
+	headers = NewHeaders()
+	data = []byte("\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, 0, len(headers))
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
+
+	// Test: invalid header spacing
+	data = []byte(" Host : localhost:42069\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid header name
+	data = []byte("Host@: localhost:42069\r\n")
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
